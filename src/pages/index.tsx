6859b1636a8da880e0c0be7b94287983b3938fa8
DwiNaufalAprialdhi <dwinaufalaprialdhi@gmail.com>
2025-05-26 16:57:25 +0700

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,27 +32,6 @@ export default function Index() {
             Untuk langkah yang insyaAllah diberkahi, untuk hati yang saling menjaga, dan cinta yang ingin pulang pada tujuan yang suci.
           </p>
 
-          {/* SWIPER */}
-          <div className='w-full grid grid-cols-1 mb-2'>
-            <Swiper
-              slidesPerView={1.1}
-              spaceBetween={10}
-              pagination={{
-                clickable: true,
-              }}
-              modules={[Pagination]}
-              className="carousel-swiper col-span-1 w-full md:h-[530px] h-[420px]"
-            >
-              {[1, 2, 3, 4].map((num) => (
-                <SwiperSlide key={num}>
-                  <div className='w-full h-max rounded-[8px] overflow-hidden'>
-                    <Image src={`/${num}.png`} alt="carousel" width={500} height={500} className='w-full h-full object-cover' />
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
-
           {/* Button Play */}
           {!isPlaying && (
             <button
@@ -71,6 +50,67 @@ export default function Index() {
               audioRef.current?.play(); // auto play ulang
             }}
           />
+
+          {/* SWIPER */}
+          <div className='w-full grid grid-cols-1 mb-2'>
+            <Swiper
+              slidesPerView={1.1}
+              spaceBetween={10}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Pagination]}
+              className="carousel-swiper col-span-1 w-full md:h-[530px] h-[435px]"
+            >
+              <SwiperSlide>
+                <div className='w-full h-max rounded-[8px] overflow-hidden'>
+                  <Image src={`/5.png`} alt="carousel" width={500} height={500} className='w-full h-full object-cover' />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='w-full h-max rounded-[8px] overflow-hidden'>
+                  <Image src={`/6.png`} alt="carousel" width={500} height={500} className='w-full h-full object-cover' />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='w-full h-max rounded-[8px] overflow-hidden'>
+                  <Image src={`/7.png`} alt="carousel" width={500} height={500} className='w-full h-full object-cover' />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='w-full h-max rounded-[8px] overflow-hidden'>
+                  <Image src={`/8.png`} alt="carousel" width={500} height={500} className='w-full h-full object-cover' />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className='w-full h-max rounded-[8px] overflow-hidden'>
+                  <Image src={`/9.png`} alt="carousel" width={500} height={500} className='w-full h-full object-cover' />
+                </div>
+              </SwiperSlide>
+            </Swiper>
+          </div>
+
+          {/* SWIPER */}
+          <div className='w-full grid grid-cols-1 mb-2'>
+            <Swiper
+              slidesPerView={1.1}
+              spaceBetween={10}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Pagination]}
+              className="carousel-swiper col-span-1 w-full md:h-[530px] h-[435px]"
+            >
+              {[1, 2, 3, 4].map((num) => (
+                <SwiperSlide key={num}>
+                  <div className='w-full h-max rounded-[8px] overflow-hidden'>
+                    <Image src={`/${num}.png`} alt="carousel" width={500} height={500} className='w-full h-full object-cover' />
+                  </div>
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+
           <p className='font-helvetica font-normal text-slate-400 text-xs leading-tight'>
             <span className='text-[#394475]'>Yuk kita berjuang bareng,</span> saling bimbing menuju ibadah yang lebih sempurna, dan saling dekatkan diri kepada Allah dalam satu tujuan, satu ridho, satu cinta.
           </p>
